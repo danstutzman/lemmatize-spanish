662c99d387d56e9b27552a309097e741e4476812
Dan Stutzman <dan@danstutzman.com>
2016-01-09 15:33:41 -0700

    Disable Named Entity Recognition.

--- setup.sh
+++ setup.sh
@@ -16,3 +16,8 @@ autoreconf --install
 env LDFLAGS="-L/opt/local/lib -L/opt/local/lib/db46 -L/usr/local/opt/icu4c/lib" CPPFLAGS="-I/opt/local/include -I/opt/local/include/boost -I/opt/local/include/db46 -I/usr/local/Cellar/boost/1.59.0/include -I/usr/local/opt/icu4c/include" ./configure --enable-boost-locale
 make 
 make install
+
+if [ ! -e myfreeling/data/config/es.cfg.bak ]; then
+  cp myfreeling/data/config/es.cfg myfreeling/data/config/es.cfg.bak
+fi
+cat myfreeling/data/config/es.cfg.bak | sed "s/NERecognition=yes/NERecognition=no/" > myfreeling/data/config/es.cfg
